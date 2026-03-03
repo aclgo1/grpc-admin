17d91de2341e1f874fa327a37162278ebc644ffd
Marcelo Santos <meusocoro5@gmail.com>
2026-03-03 13:56:51 -0300

    added max idle conns max opens conns

--- pkg/postgres/connect.go
+++ pkg/postgres/connect.go
@@ -3,6 +3,7 @@ package postgres
 import (
 	"context"
 	"log"
+	"time"
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
@@ -17,6 +18,10 @@ func Connect(dbDriver, dbUri string) *sqlx.DB {
 	if err := conn.PingContext(context.Background()); err != nil {
 		log.Fatalf("sqlx.PingContext: %v", err)
 	}
+
+	conn.SetMaxIdleConns(15)
+	conn.SetMaxOpenConns(25)
+	conn.SetConnMaxLifetime(time.Minute * 5)
 
 	return conn
 }
